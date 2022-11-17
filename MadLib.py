e14d3e91c17f2124f433d4d7070384405a5c431b
KingOfSteves1 <90356729+KingOfSteves1@users.noreply.github.com>
2022-11-17 10:05:05 -0700

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,7 @@
+verb1 = input("Type a verb with ing here: ")
+adjective1 = input("Type a Adjective with y in it here: ")
+noun1 = input("Type a person's name here: ")
+noun2 = input("Type a secound noun here: ")
+verb2 = input("Type a secound verb here with a ly: ")
+verb3 = input("Type a verb that ends with ed")
+print("It was a dark and "+adjective1+" night. It was at "+noun1+"'s House. They were "+verb1+". Then they threw a "+noun2+" at you. You "+verb2+" dodged it. You "+verb3+" at "+noun1+", 'Why "+noun1+"!'")

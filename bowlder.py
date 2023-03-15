85e664c2227679be662aee93677fb66ea41697a3
KingOfSteves1 <90356729+KingOfSteves1@users.noreply.github.com>
2023-03-15 10:08:05 -0600

    Add files via upload

---
+++ bowlder.py
@@ -0,0 +1,21 @@
+avatar = {
+    "earth":{
+        "name1":"toph",
+        "name2":"the bowlder"
+    },
+    "water":{
+        "name3":"katara"
+    },
+    "nobending":{
+        "name4":"sokka"
+    },
+    "fire":{
+        "name5":"zuko",
+        "name6":"azula"
+    },
+    "air":{
+        "name7":"apa",
+        "name8":"aang"
+    },
+}
+print(avatar["air"]["name8"])
